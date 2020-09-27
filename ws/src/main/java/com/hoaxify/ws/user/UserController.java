0138e8a038c49c2cb6803930d7e17d60b9d9f110
Nozcepni <53128374+Nozcepni@users.noreply.github.com>
2020-09-27 21:04:04 +0300

    Postu alma kısmında tek tek kendimiz username'i falan kontrol etmek yerine daha önce kullanmış olduğumuz bean validation --not null falan-- özelliklerini kullandık ve exceptionhandler annotationuyla beraber belirtmiş olduğumuz özellikleri yakaladık response döndürdük

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -5,12 +5,18 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hoaxify.ws.error.ApiError;
@@ -25,32 +31,29 @@ public class UserController {
 	//private static final Logger Log = LoggerFactory.getLogger(UserController.class);
 
 	@PostMapping("/api/1.0/users/")
-	public ResponseEntity<?> createUser(@RequestBody User user) {
-		
+	public GenericResponse createUser(@Valid @RequestBody User user) {
+		System.out.println("sdfsfd");
+		userService.save(user);
+		return (new GenericResponse("user created"));
+	
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiError handleValidationException(MethodArgumentNotValidException exception) {
+		System.out.println("sd");
 		ApiError error = new ApiError(400,"Validation error","/api/1.0/users");
 		Map<String,String> validationErrors = new HashMap<>();
-		
-		String username = user.getUsername();
-		String displayname = user.getDisplayname();
-		
-		if(username==null || username.isEmpty()) {
-			validationErrors.put("username","Username cannot be null");
-			
+
+		for(FieldError fieldError: exception.getBindingResult().getFieldErrors()) {
+			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
 		}
 		
-		if(displayname==null || displayname.isEmpty()) {
-			validationErrors.put("displayname","Displayname cannot be null");
-		}
+		error.setValidationErrors(validationErrors);
+
+		return error;
 		
-		if(validationErrors.size()>0) {
-			error.setValidationErrors(validationErrors);
-			
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
-		}
 		
-		userService.save(user);
-		return ResponseEntity.ok(new GenericResponse("user created"));
-	
 	}
 	
 }
