bbad677063c7970547baad29596ff5efe544a732
Nozcepni <53128374+Nozcepni@users.noreply.github.com>
2020-09-29 20:24:03 +0300

    login ve signup icin errorhandlerı ortak bir sınıf haline getirdik

--- ws/src/main/java/com/hoaxify/ws/error/ErrorHandler.java
+++ ws/src/main/java/com/hoaxify/ws/error/ErrorHandler.java
@@ -1,10 +1,13 @@
 package com.hoaxify.ws.error;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
@@ -24,7 +27,24 @@ public class ErrorHandler implements ErrorController {
 		String path = (String)attributes.get("path");
 		int status = (Integer)attributes.get("status");
 		
-		return new ApiError(status, message, path);
+		ApiError error = new ApiError(status, message, path);
+		
+		if(attributes.containsKey("errors")) {
+			
+			List<FieldError> fieldErrors = (List<FieldError>) attributes.get("errors");
+			
+			Map<String,String> validationErrors = new HashMap<>();
+
+			for(FieldError fieldError: fieldErrors) {
+				validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+			}
+			
+					
+			error.setValidationErrors(validationErrors);
+			
+		}	
+		
+		return error;
 		
 	}
 	
