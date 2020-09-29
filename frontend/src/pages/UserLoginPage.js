e4d63d7d0ad16028e08858ff9e59e399786b8841
Nozcepni <53128374+Nozcepni@users.noreply.github.com>
2020-09-29 22:01:24 +0300

    Login fail olması durumunda hata mesajını göstermek icin clientı düzenledik

--- frontend/src/pages/UserLoginPage.js
+++ frontend/src/pages/UserLoginPage.js
@@ -1,73 +1,77 @@
 import React, { Component } from 'react'
 import Input from '../components/Input'
-import {login} from '../api/apiCalls'
+import { login } from '../api/apiCalls'
 
 class UserLoginPage extends Component {
 
-    state={
-        username:null,
-        password:null,
-        isEmpty:true,
+    state = {
+        username: null,
+        password: null,
+        error: null
     }
 
-    
-    onChange = (event)=>{
 
-        const {username,password} = this.state
+    onChange = (event) => {
 
-        const{name,value} = event.target
+        const { name, value } = event.target
 
         this.setState({
-            [name] : value 
+            [name]: value,
+            error:null
         })
-
-        if( (username==null || password==null) ){
-            this.setState({
-                isEmpty:true
-            })
-        }
-        else{
-            this.setState({
-                isEmpty:false
-            })
-        }
 
     }
 
-    onClickLogin = async (event) =>{
+    onClickLogin = async (event) => {
 
         event.preventDefault();
 
+        this.setState({
+            error:null
+        })
+
         const creds = {
-            username:this.state.username,
-            password:this.state.password
+            username: this.state.username,
+            password: this.state.password
         }
 
-        login(creds)
-
-        this.setState({ pendingApiCall: false })
+        try {
+            await login(creds)
+        }
+        catch (apiError) {
+            this.setState({
+                error: apiError.response.data.message
+            })
+        }
 
     }
 
     render() {
 
-        const {isEmpty,pendingApiCall} = this.state
+        const { pendingApiCall,error,username,password } = this.state
+
+        const isEmpty= username && password;
 
         return (
             <div className="container">
 
                 <div className="col text-center" style={{ marginBottom: "100px,", marginTop: "20px" }}>
-                        <h1> Login</h1>
-                    </div>
-
-              
+                    <h1> Login</h1>
+                </div>
 
                 <Input onChange={this.onChange} name="username" label="Username"></Input>
                 <Input onChange={this.onChange} name="password" label="Password" type="password"></Input>
 
-                <div className="col text-center">
-                    <button onClick={this.onClickLogin} style={{marginTop:"25px",background:"purple",border:"purple"}} className="btn btn-info " disabled={isEmpty}>Login</button>
+                <div className="text-center">
+                { error && <div style={{marginTop:'25px', width:'20%', marginLeft:'450px'}} class="alert alert-danger" role="alert">
+                      {error}
+                </div>}
                 </div>
+
+                <div className="col text-center">
+                    <button onClick={this.onClickLogin} style={{ marginTop: "25px", background: "purple", border: "purple" }} className="btn btn-info " disabled={!isEmpty}>Login</button>
+                </div>
+
 
             </div>
         )
